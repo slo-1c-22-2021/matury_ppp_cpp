15ff8639a6b686769aece9c304bae9b3443d3ff0
slo-1c-22-2021 <81530845+slo-1c-22-2021@users.noreply.github.com>
2022-03-24 07:44:18 +0100

    Create 2010_ppp_zadnie_4.cpp

---
+++ 2010_ppp_zadnie_4.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool is_palindrome(string s)
+{
+    int p,k;
+    p=0;
+    k=s.size()-1;
+while(p<k){
+    if(s[p] !=s[k]) return false;
+    p++;
+    k--;
+}
+return true;
+}
+int main(){
+int wynik;
+int licznik=0;
+string s;
+ifstream f("dane.txt");
+while(f>>s){
+    cout<<s<<"\n";
+}
+if(is_palindrome(s)) licznik++;
+cout<<s<<"\n";
+}
+cout<<"wynik = "<<licznik<<"\n";
+
